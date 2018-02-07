f1892b2b7a50757f36fb763f5b394a958c911bb5
sadheeshkt <35289037+sadheeshkt@users.noreply.github.com>
2018-02-07 13:21:56 +0530

    Update Classes.groovy

--- Classes.groovy
+++ Classes.groovy
@@ -1,1 +1,17 @@
+class URLConfig{
+  String repo
+  String url
+  String pomPath
+  
+}
 
+class B{
+    def greet(name){ return "greet from B: $name!" }
+}
+
+// this method just to have nice access to create class by name
+Object getProperty(String name){
+    return this.getClass().getClassLoader().loadClass(name).newInstance();
+} 
+
+return this
